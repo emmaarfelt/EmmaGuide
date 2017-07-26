2917312582c59a39e2db317440c28132709dcd03
Emma Arfelt Kock <ekoc@itu.dk>
2017-07-26 16:39:13 +0200

    Bigger refactoring and small bug fixes

---
+++ EmmaGuide/Extensions/URLSession_extensions.swift
@@ -0,0 +1,32 @@
+//
+//  URLSession_extensions.swift
+//  
+//
+//  Created by Emma Arfelt Kock on 26/07/2017.
+//
+
+import Foundation
+
+
+extension URLSession {
+    func synchronousDataTask(with url: URL) -> (Data?, URLResponse?, Error?) {
+        var data: Data?
+        var response: URLResponse?
+        var error: Error?
+        
+        let semaphore = DispatchSemaphore(value: 0)
+        
+        let dataTask = self.dataTask(with: url) {
+            data = $0
+            response = $1
+            error = $2
+            
+            semaphore.signal()
+        }
+        dataTask.resume()
+        
+        _ = semaphore.wait(timeout: .distantFuture)
+        
+        return (data, response, error)
+    }
+}
